8c80e5147caa6450750407fd4375dc8d33cb2c65
sammy <sammylaureys@hotmail.com>
2020-10-05 19:45:37 +0200

    start of layout

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import styled from "@emotion/styled";
+
+const WhiteP = styled.h1`
+  color: white;
+`;
+
+const StyledBackground1 = styled.div`
+  background-color: #323633;
+  border-radius: 10px;
+  box-shadow: 0 0 50px 5px #0ff;
+  margin: 5% 8%;
+  padding: 0.5%;
+  text-align: center;
+`;
+
+const StyledBackground2 = styled.div`
+  background-color: #323633;
+  border-radius: 10px;
+  box-shadow: 0 0 50px 1px #0ff;
+  margin: 5% 12%;
+  padding: 0.5%;
+  text-align: center;
+  height: 500px;
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <body>
+          <StyledBackground1>
+              <WhiteP><strong>TO DO LIST</strong></WhiteP>
+          </StyledBackground1>
+          <StyledBackground2>
+          </StyledBackground2>
+        </body>
+    );
 }
 
 export default App;
